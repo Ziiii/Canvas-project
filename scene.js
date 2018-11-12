7fba81638c257992bd0bf0f086dee1b2b88b12e9
Larin Anton <a.larin@itransition.com>
2018-11-12 20:05:24 +0300

    scene sizing added

--- scene.js
+++ scene.js
@@ -1,21 +1,29 @@
 class Scene {
-    constructor(canvasId){
+    constructor(canvasId) {
         this.canvas = document.getElementById(canvasId);
         this.context = this.canvas.getContext('2d');
+        this.resize();
+        this.resize = this.resize.bind(this);
+        window.onresize = this.resize;
     }
 
-    drawRect(rect){
+    drawRect(rect) {
         this.context.fillStyle = rect.color;
-        this.context.fillRect(rect.x1,rect.y1,rect.width,rect.height);
+        this.context.fillRect(rect.x1, rect.y1, rect.width, rect.height);
     }
 
-    drawRects(rects){
-        for(let rect of rects){
+    drawRects(rects) {
+        for (let rect of rects) {
             this.drawRect(rect);
         }
     }
 
-    clearScene(){
-        this.context.clearRect(0,0,1000,600)
+    resize() {
+        this.canvas.height = window.screen.height;
+        this.canvas.width = window.screen.width;
+    }
+
+    clearScene() {
+        this.context.clearRect(0, 0, 1000, 600)
     }
 }
